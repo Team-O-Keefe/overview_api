729d53f29137c1eb6e35f4e762a9b32b9d14ff2c
Erin O'Connor <oconnor.wa@gmail.com>
2021-08-16 18:56:15 -0700

    added conditional to products endpoint to set page and count defaults, added response status to res.status for errors, added async/await to getProducts function

--- server/overview.js
+++ server/overview.js
@@ -3,55 +3,20 @@ const DBCONFIG = require('./.db-config');
 
 const pool = new Pool(DBCONFIG.db);
 
-module.exports.getProducts = (req, res) => {
-  const values = [
-    req.query.page,
-    req.query.count
-  ];
+module.exports.getProducts = async (page, count, cb) => {
   const query = `SELECT *
                   FROM products
-                  WHERE product_id BETWEEN $1 AND $2`;
-  pool.connect()
-    .then((client) => {
-      client.query(query, values)
-        .then((response) => {
-          client.release();
-          res.send(response.rows);
-        })
-        .catch((error) => {
-          client.release();
-          res.status(error);
-          res.end();
-        });
+                  WHERE product_id BETWEEN ${page} AND ${count * page}`;
+  await pool.query(query)
+    .then((response) => {
+      cb(null, response.rows);
     })
     .catch((error) => {
-      res.status(error);
-      res.end();
+      cb(error, null);
     });
 };
 
 module.exports.getProductInfo = async (pId, cb) => {
-  // const query = `SELECT
-  //                 json_build_object(
-  //                   'style_id', styles.style_id,
-  //                   'product_id', styles.product_id,
-  //                   'name', styles.name,
-  //                   'sale_price', styles.sale_price,
-  //                   'original_price', styles.original_price,
-  //                   'default?', styles.default_style,
-  //                   'photos', json_agg(
-  //                          json_build_object(
-  //                            'url', photos.url, 'thumbnail_url', photos.thumbnail_url
-  //                          )
-  //                        ),
-  //                 ) AS results
-  //                   FROM products, styles, photos
-  //                   WHERE products.product_id = 10
-  //                   AND products.product_id = styles.product_id
-  //                   AND styles.style_id = photos.style_id
-  //                   AND styles.style_id = skus.style_id
-  //                   GROUP BY styles.style_id;`;
-
   const query2 = `SELECT
                     product_id, json_agg(json_build_object(
                       'style_id', style_id,
@@ -81,24 +46,9 @@ module.exports.getProductInfo = async (pId, cb) => {
 
   pool.query(query2)
     .then((response) => {
-      console.log(response.rows[0]);
       cb(null, response.rows[0]);
     })
     .catch((error) => {
-      console.error(`ERROR ::: ${error}`);
+      cb(error, null);
     });
-  // await pool.connect()
-  //   .then((client) => {
-  //     client.query(query2, pId)
-  //       .then((response) => {
-  //         client.release();
-  //       })
-  //       .catch((error) => {
-  //         client.release();
-  //         console.error(`ERROR ::: ${error}`);
-  //       });
-  //   })
-  //   .catch((error) => {
-  //     console.error(`ERROR ::: ${error}`);
-  //   });
 };
--- server/router.js
+++ server/router.js
@@ -1,15 +1,30 @@
 const router = require('express').Router();
 const controller = require('./overview');
 
-router.get('/products', controller.getProducts);
+router.get('/products', (req, res) => {
+  let { page, count } = req.query;
+
+  if (!page) {
+    page = 1;
+  }
+  if (!count) {
+    count = 5;
+  }
+  controller.getProducts(page, count, (error, data) => {
+    if (error) {
+      res.status(400);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 router.get('/products/:product_id/styles', (req, res) => {
   const id = req.params.product_id;
   controller.getProductInfo(id, (error, data) => {
     if (error) {
-      res.status();
+      res.status(400);
     } else {
-      console.log(data);
       res.send(data);
     }
   });
